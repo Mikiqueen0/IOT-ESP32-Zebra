7bf0dfc1343d3e774d96b3069d590ffa1efcfe65
Mikiqueen0 <107250330+Mikiqueen0@users.noreply.github.com>
2024-12-19 23:10:01 +0700

    Add files via upload

---
+++ ESP32CAM.cpp
@@ -0,0 +1,383 @@
+/*********
+  Rui Santos
+  Complete instructions at: https://RandomNerdTutorials.com/esp32-cam-save-picture-firebase-storage/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  Based on the example provided by the ESP Firebase Client Library
+*********/
+
+#include "Arduino.h"
+#include "WiFi.h"
+#include "esp_camera.h"
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+#include <LittleFS.h>
+#include <FS.h>
+#include <SD.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include <addons/TokenHelper.h>
+#include <esp_now.h>
+#include "time.h"
+
+typedef struct struct_message {
+  tm time;
+  bool status;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message data;
+
+// change to your own wifi
+const char* ssid = "me";
+const char* password = "23456788";
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCgCPwravCD0z1yA2-zBn8BhD5tGLQ2Egc"
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "iotzebraimage@gmail.com"
+#define USER_PASSWORD "1212312121"
+
+// Insert Firebase storage bucket ID e.g bucket-name.appspot.com
+#define STORAGE_BUCKET_ID "iot-zebra-a7889.firebasestorage.app"
+// For example:
+//#define STORAGE_BUCKET_ID "esp-iot-app.appspot.com"
+
+// Remove static file names and use dynamic naming
+char dynamicLocalFilePath[40];  // Dynamic local file path for photo
+char dynamicRemoteFilePath[40]; // Dynamic remote file path for Firebase
+
+// OV2640 camera module pins (CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define ONBOARDLED 33
+
+boolean takeNewPhoto = true;
+
+//Define Firebase Data objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig configF;
+
+// configF.rtos.timeout = 60000;
+// configF.timeout.socketConnection = 30000; // Set to 30 seconds
+
+
+void fcsUploadCallback(FCS_UploadStatusInfo info);
+
+bool taskCompleted = false;
+
+char timeBuffer[30];
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  memcpy(&data, incomingData, sizeof(data));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  
+  // Format the time structure into a string
+  strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d_%H-%M-%S", &data.time);
+  Serial.print("Time: ");
+  Serial.println(timeBuffer);
+  
+  Serial.print("Status: ");
+  Serial.println(data.status ? "true" : "false");
+  Serial.println();
+}
+
+void capturePhotoSaveLittleFS() {
+  // Dispose first pictures because of bad quality
+  camera_fb_t* fb = NULL;
+  // Skip first 3 frames (increase/decrease number as needed).
+  for (int i = 0; i < 4; i++) {
+    fb = esp_camera_fb_get();
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
+    
+  // Take a new photo
+  fb = NULL;  
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }  
+
+  // Create dynamic file name using timeBuffer
+  snprintf(dynamicLocalFilePath, sizeof(dynamicLocalFilePath), "/%s.jpg", timeBuffer);  // Dynamic local file name
+
+  // Save the photo to LittleFS
+  File file = LittleFS.open(dynamicLocalFilePath, FILE_WRITE);
+  // File file = LittleFS.open("/photo.jpg", FILE_WRITE);
+
+  // Insert the data in the photo file
+  if (!file) {
+    Serial.println("Failed to open file in writing mode");
+  }
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.print("The picture has been saved in ");
+    Serial.print(dynamicLocalFilePath);
+    // Serial.print("/photo.jpg");
+    Serial.print(" - Size: ");
+    Serial.print(fb->len);
+    Serial.println(" bytes");
+  }
+  // Close the file
+  file.close();
+  esp_camera_fb_return(fb);
+}
+
+
+void initWiFi(){
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(ONBOARDLED, HIGH);
+    delay(500);
+    digitalWrite(ONBOARDLED, LOW);
+    WiFi.reconnect();
+    attempts++;
+    if (attempts > 20) {  // Timeout after 20 seconds
+      Serial.println("\nFailed to connect to WiFi. Restarting...");
+      ESP.restart();
+    }
+  }
+  Serial.println("\nConnected to WiFi!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(ONBOARDLED, HIGH);
+}
+
+
+void initLittleFS(){
+  if (!LittleFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    ESP.restart();
+  }
+  else {
+    delay(500);
+    Serial.println("LittleFS mounted successfully");
+  }
+}
+
+void initCamera(){
+ // OV2640 camera module
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 6;
+    config.fb_count = 1;
+  } 
+  // else {
+  //   config.frame_size = FRAMESIZE_SVGA;
+  //   config.jpeg_quality = 12;
+  //   config.fb_count = 1;
+  // }
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  } 
+}
+
+void setup() {
+  pinMode(ONBOARDLED, OUTPUT);
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  initWiFi();
+  initLittleFS();
+  // Turn-off the 'brownout detector'
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  initCamera();
+
+  //Firebase
+  // Assign the api key
+  configF.api_key = API_KEY;
+  //Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  //Assign the callback function for the long running token generation task
+  configF.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  Firebase.begin(&configF, &auth);
+  Firebase.reconnectWiFi(true);
+
+  //Communitate with another esp board
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+  Serial.println("Finished setup()");
+
+  formatLittleFS();
+
+  File root = LittleFS.open("/");
+  File file = root.openNextFile();
+  while (file) {
+    Serial.println(file.name());
+    file = root.openNextFile();
+  }
+
+}
+
+void formatLittleFS() {
+  Serial.println("Formatting LittleFS...");
+  if (LittleFS.format()) {
+    Serial.println("LittleFS formatted successfully!");
+  } else {
+    Serial.println("Failed to format LittleFS.");
+  }
+}
+
+
+#define WIFI_SIGNAL_THRESHOLD -90  // Set this value to your preferred RSSI threshold (in dBm)
+#define MAX_RETRIES 5  // Set the maximum number of retries for upload attempts
+
+int retryCount = 0;  // Counter for retry attempts
+
+void loop() {
+  // // Check Wi-Fi signal strength
+  int rssi = WiFi.RSSI();
+  // Serial.print("Wi-Fi RSSI: ");
+  // Serial.println(rssi);
+
+  if (rssi < WIFI_SIGNAL_THRESHOLD) {  // If the Wi-Fi signal is weak
+    Serial.print("Weak Wi-Fi signal, retrying... Wi-Fi RSSI: ");
+    Serial.println(rssi);
+    delay(2000);  // Wait a moment before retrying (you can adjust this delay as needed)
+    return;  // Skip the upload if the signal is weak and retry later
+  }
+
+  if (data.status) {
+    capturePhotoSaveLittleFS();  // Capture and save photo
+    listFiles(); // See all files
+    data.status = false;
+    delay(1);
+
+    if (Firebase.ready() && !taskCompleted) {
+      taskCompleted = true;
+      Serial.print("Uploading picture... ");
+
+      // Upload to Firebase Storage with dynamic file paths
+      snprintf(dynamicRemoteFilePath, sizeof(dynamicRemoteFilePath), "/data/%s.jpg", timeBuffer);  // Dynamic remote file name
+
+      // Try uploading to Firebase Storage
+      bool uploadSuccess = Firebase.Storage.upload(&fbdo, STORAGE_BUCKET_ID, dynamicLocalFilePath, mem_storage_type_flash, dynamicRemoteFilePath, "image/jpeg", fcsUploadCallback);
+
+      if (uploadSuccess) {
+        Serial.printf("\nDownload URL: %s\n", fbdo.downloadURL().c_str());
+        LittleFS.remove(dynamicLocalFilePath);  // Delete file after successful upload
+        Serial.println("File deleted to free up space.");
+        retryCount = 0;  // Reset retry count after successful upload
+        taskCompleted = false;
+      } else {
+        Serial.println(fbdo.errorReason());  // Error message if upload fails
+        retryCount++;  // Increment retry count
+
+        if (retryCount >= MAX_RETRIES) {
+          Serial.println("Upload failed after maximum retries. Stopping upload attempts.");
+          retryCount = 0;  // Reset the retry count for future attempts
+          taskCompleted = false;
+        } else {
+          Serial.println("Retrying upload...");
+        }
+      }
+    }
+  }
+}
+
+void listFiles() {
+  File root = LittleFS.open("/");
+  File file = root.openNextFile();
+  Serial.println("File lists");
+  while (file) {
+    Serial.println(file.name());
+    file = root.openNextFile();
+  }
+  Serial.println("---------");
+}
+// The Firebase Storage upload callback function
+void fcsUploadCallback(FCS_UploadStatusInfo info){
+    if (info.status == firebase_fcs_upload_status_init){
+        Serial.printf("Uploading file %s (%d) to %s\n", info.localFileName.c_str(), info.fileSize, info.remoteFileName.c_str());
+    }
+    else if (info.status == firebase_fcs_upload_status_upload)
+    {
+        Serial.printf("Uploaded %d%s, Elapsed time %d ms\n", (int)info.progress, "%", info.elapsedTime);
+    }
+    else if (info.status == firebase_fcs_upload_status_complete)
+    {
+        Serial.println("Upload completed\n");
+        FileMetaInfo meta = fbdo.metaData();
+        Serial.printf("Name: %s\n", meta.name.c_str());
+        Serial.printf("Bucket: %s\n", meta.bucket.c_str());
+        Serial.printf("contentType: %s\n", meta.contentType.c_str());
+        Serial.printf("Size: %d\n", meta.size);
+        Serial.printf("Generation: %lu\n", meta.generation);
+        Serial.printf("Metageneration: %lu\n", meta.metageneration);
+        Serial.printf("ETag: %s\n", meta.etag.c_str());
+        Serial.printf("CRC32: %s\n", meta.crc32.c_str());
+        Serial.printf("Tokens: %s\n", meta.downloadTokens.c_str());
+        Serial.printf("Download URL: %s\n\n", fbdo.downloadURL().c_str());
+    }
+    else if (info.status == firebase_fcs_upload_status_error){
+        Serial.printf("Upload failed, %s\n", info.errorMsg.c_str());
+    }
+}
---
+++ ESP32DevModule.cpp
@@ -0,0 +1,485 @@
+#include <ESP32Servo.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include "time.h"
+#include <Arduino.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_SH110X.h>
+
+// Change to your own wifi
+const char* ssid = "me";
+const char* password = "23456788";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 7 * 60 * 60;
+const int   daylightOffset_sec = 0;
+
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x2C, 0xBC, 0xBB, 0x85, 0x4A, 0xA4};
+
+typedef struct struct_message {
+  tm time;
+  bool status;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message data;
+
+esp_now_peer_info_t peerInfo;
+
+// State variables
+bool buttonPressed = false;
+unsigned long interval = 1000; // 1 second
+
+// Constants
+const int COUNTDOWN_CAR = 2+1;
+const int COUNTDOWN_PEDESTRIAN = 2;
+const int COUNTDOWN_YELLOW = 2+1;
+
+// Define pins for ultrasonic sensor
+const int trigPin = 12;
+const int echoPin = 14;
+
+// IR sensor
+const int IRPin = 26;
+
+// Servo motor pin
+Servo myServo;
+
+// Define LED pins for traffic lights
+const int greenLedCar = 18;
+const int yellowLedCar = 5;
+const int redLedCar = 17;
+const int greenLedPedestrian = 16;
+const int redLedPedestrian = 4;
+
+const int onBoardLED = 2;
+
+// Define button pins
+const int buttonPin1 = 15;
+
+const int buzzer = 25;
+
+static int countdownCar = COUNTDOWN_CAR;
+static int countdownPedestrian = COUNTDOWN_PEDESTRIAN;
+static int state = 0;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+//#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+Adafruit_SH1106G oled = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
+const int servoOpen = 90;
+const int servoClose = 30;
+
+// Setup function
+void setup() {
+  // Wire.setClock(100000); // Set I2C to 100 kHz
+
+  // Initialize LEDs as OUTPUT
+  pinMode(greenLedCar, OUTPUT);
+  pinMode(yellowLedCar, OUTPUT);
+  pinMode(redLedCar, OUTPUT);
+  pinMode(greenLedPedestrian, OUTPUT);
+  pinMode(redLedPedestrian, OUTPUT);
+
+  pinMode(onBoardLED, OUTPUT);
+
+  // Initialize buttons as INPUT with pull-up resistors
+  pinMode(buttonPin1, INPUT_PULLUP);
+
+  // Initialize LCDs
+  // lcdCar.init();
+  // lcdCar.backlight();
+  
+  delay(1000); // wait for the OLED to power up
+  oled.begin(i2c_Address, true); // Address 0x3C default
+
+  oled.setTextSize(2);
+  oled.setTextColor(SH110X_WHITE);
+  oled.setCursor(0, 0);
+  oled.println("Starting...");
+  oled.display();
+
+  delay(2000);
+
+  // Clear the buffer.
+  oled.clearDisplay();
+  oled.display();
+
+  // Initialize ultrasonic sensor pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+
+  // ledcSetup(0, 1000, 8);       // Set up PWM channel 0 with 1 kHz frequency
+  // ledcAttachPin(buzzer, 0);
+  // ledcWriteTone(0, 0);
+  pinMode(buzzer, OUTPUT);
+  // delay(50);
+  digitalWrite(buzzer, HIGH);
+
+  // Initialize IR sensor pins
+  pinMode(IRPin,INPUT);
+
+  // Initialize servo motor
+  myServo.write(servoOpen);
+  myServo.attach(19);
+  delay(1000);
+  // myServo.write(90); // Start with barrier open
+
+  // Start serial communication
+  //Serial.begin(9600);
+
+  // Start in initial state
+  resetToDefaultState();
+
+  //// Wifi setup
+  Serial.begin(115200);
+
+  // Connect to esp32cam
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Transmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  int retry = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+    digitalWrite(onBoardLED, HIGH);
+    delay(250);
+    digitalWrite(onBoardLED, LOW);
+    retry++;
+    if (retry >= 15) {
+      ESP.restart();
+    }
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  digitalWrite(onBoardLED, HIGH);
+  
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  localTime();
+
+  // //disconnect WiFi as it's no longer needed
+  // WiFi.disconnect(true);
+  // WiFi.mode(WIFI_OFF);
+}
+
+static unsigned int temp;
+void loop() {
+  unsigned long currentMillis = millis(); // Use this as the main reference for time
+  
+  switch (state) {
+    case 0: // button not presssed
+      setCarLight(0);
+      setPedestrianLight(2);
+      if (digitalRead(buttonPin1) == HIGH && !buttonPressed) {
+        buttonPressed = true;
+        countdownCar = COUNTDOWN_CAR;
+        countdownPedestrian = COUNTDOWN_CAR + COUNTDOWN_YELLOW;
+        state = 1;
+      }
+      break;
+    case 1: // pressed
+      nonBlockingCountdown(countdownCar, countdownPedestrian, currentMillis);
+      if (countdownCar == 0) {
+        countdownCar = COUNTDOWN_YELLOW;
+        state = 2;
+      }
+      break;
+    case 2: // yellow
+      setCarLight(1);
+      nonBlockingCountdown(countdownCar, countdownPedestrian, currentMillis);
+      if (countdownCar == 0) {
+        state = 3;
+        countdownCar = COUNTDOWN_PEDESTRIAN + COUNTDOWN_YELLOW;
+        countdownPedestrian = COUNTDOWN_PEDESTRIAN + COUNTDOWN_YELLOW;
+        temp = countdownPedestrian;
+      }
+      break;
+    case 3: // ped
+      
+      setCarLight(2);
+      setPedestrianLight(0);
+      myServo.write(servoClose);
+      isCarCross(currentMillis);
+      // if(countdown(2, currentMillis)) {
+      //   // ledcWrite(0, 0);
+      //   digitalWrite(buzzer, HIGH);
+      //   // ledcWriteTone(0, 1000); // Generate a 1 kHz tone
+      //   // delay(2000); 
+      // }
+      // else {
+      //   // ledcWriteTone(0, 0);
+      //   digitalWrite(buzzer, LOW);
+      // }
+      // if (currentMillis)
+      nonBlockingCountdown(countdownCar, countdownPedestrian, currentMillis);
+      if (countdownPedestrian > temp - 2) {
+        Serial.println(countdownPedestrian);
+        digitalWrite(buzzer, LOW);
+      } else {
+        digitalWrite(buzzer, HIGH);
+      }
+      if (countdownPedestrian == 0) {
+        state = 4;
+      }
+      break;
+    case 4: // green blink
+      isCarCross(currentMillis);
+      blinkingGreenLightState(currentMillis);
+      nonBlockingCountdown(countdownCar, countdownPedestrian, currentMillis);
+      if (countdownPedestrian == 0) {
+        if (isCrosswalkOccupied(currentMillis)) {
+          blinkingGreenLightState(currentMillis);
+          oled.clearDisplay();
+          oled.setTextSize(1);
+          oled.setTextColor(SH110X_WHITE);
+          oled.setCursor(0, 0);
+          oled.println("Waiting for people");
+          oled.println("to leave...");
+          oled.display();
+
+          // Serial.println("WAITING...");
+        } else {
+          resetToDefaultState();
+          // Serial.println("Not WAITING ANYMORE");
+        }
+      }
+      break;
+  }
+}
+
+bool countdown (int countdown, unsigned long currentMillis) {
+  static unsigned long lastCountdown = 0;
+  if (currentMillis - lastCountdown >= countdown) {
+    lastCountdown = currentMillis;
+    return true;
+  }
+  return false;
+}
+
+void nonBlockingCountdown(int &countdownCar, int &countdownPedestrian, unsigned long currentMillis) {
+  static unsigned long lastUpdateCar = 0;
+  if (currentMillis - lastUpdateCar >= interval) {
+    lastUpdateCar = currentMillis;
+    if (countdownCar > 0 && countdownPedestrian > 0) {
+      countdownCar--;
+      countdownPedestrian--;
+      displayCountdown(countdownCar, countdownPedestrian);
+    }
+  }
+}
+
+void blinkingGreenLightState(unsigned long currentMillis) {
+    static unsigned long lastBlinkTime = 0;
+    static bool greenState = false;
+    if (currentMillis - lastBlinkTime >= interval / 4) {
+        greenState = !greenState;
+        digitalWrite(greenLedPedestrian, greenState ? HIGH : LOW);
+        lastBlinkTime = currentMillis;
+    }
+}
+
+void displayCountdown(int carCountdown, int pedestrianCountdown) {
+  int lineHeight = 32;
+  int halfLineHeight = lineHeight/2;
+  // Car
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(SH110X_WHITE);
+  oled.setCursor(0, lineHeight*0);
+  oled.println("Car Countdown");
+  oled.setTextSize(2);
+  oled.setTextColor(SH110X_WHITE);
+  oled.setCursor(0, lineHeight*0 + halfLineHeight);
+  oled.println(carCountdown);
+
+  // Pedestrian
+  oled.setTextSize(1);
+  oled.setTextColor(SH110X_WHITE);
+  oled.setCursor(0, lineHeight*1);
+  oled.println("Pedestrian Countdown");
+  oled.setTextSize(2);
+  oled.setTextColor(SH110X_WHITE);
+  oled.setCursor(0, lineHeight*1 + halfLineHeight);
+  oled.println(pedestrianCountdown);
+
+  oled.display();
+}
+
+const unsigned long ultrasonicInterval = 100;
+const int ignoreDistance = 500;
+bool isCrosswalkOccupied(unsigned long currentMillis) {
+  static unsigned long lastUltrasonicTime = 0;  
+  static long lastDistance = 0;
+  long duration, distance;
+  if (currentMillis - lastUltrasonicTime >= ultrasonicInterval) {
+    lastUltrasonicTime = currentMillis;
+    // Trigger and read ultrasonic sensor logic
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
+    // lastDistance = distance;
+    if (distance > 100) {
+      distance = lastDistance;
+    } else {
+      lastDistance = distance;
+    }
+    
+    Serial.print(distance);
+    Serial.println(" cm");
+    
+    return (distance < 13 && abs(lastDistance - distance) < 3);
+  }
+}
+
+// Add this flag to track capture state
+bool captureTaken = false;  // New variable to track capture status
+const unsigned long debounceDelay = interval; // 1-second debounce delay
+void isCarCross(unsigned long currentMillis) {
+  int val = digitalRead(IRPin);
+  static unsigned long lastCaptureTime = 0; // Store the time of the last capture
+  // static bool captureTaken = false;  // New variable to track capture status
+  
+  // unsigned long currentTime = millis();
+
+  // If a car is detected and debounce period has passed
+  if (val == LOW && (currentMillis - lastCaptureTime > debounceDelay) && !captureTaken) {
+    lastCaptureTime = currentMillis;
+    captureTaken = true;
+
+    // Set values to send
+    data.time = localTime();
+    data.status = true;
+    
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &data, sizeof(data));
+    
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+    // localTime(); // Print time
+  }
+
+  // Reset the capture flag when the sensor is clear
+  if (val == HIGH) {
+    captureTaken = false;
+  }
+}
+
+// Reset state function
+void resetToDefaultState() {
+  countdownCar = COUNTDOWN_CAR;
+  countdownPedestrian = COUNTDOWN_PEDESTRIAN;
+  buttonPressed = false;
+  myServo.write(servoOpen);  // Raise the barrier
+  setCarLight(0);  // Cars green
+  setPedestrianLight(2);  // Pedestrians red
+  clearScreen();
+  state = 0;
+  captureTaken = false; 
+  // lastCaptureTime = 0;  // Reset the capture time
+}
+
+void clearScreen() {
+  oled.clearDisplay();
+  oled.display();
+}
+
+// Set car traffic light
+void setCarLight(int colorID) {
+  digitalWrite(greenLedCar, colorID == 0 ? HIGH : LOW);
+  digitalWrite(yellowLedCar, colorID == 1 ? HIGH : LOW);
+  digitalWrite(redLedCar, colorID == 2 ? HIGH : LOW);
+}
+
+// Set pedestrian traffic light
+void setPedestrianLight(int colorID) {
+  digitalWrite(greenLedPedestrian, colorID == 0 ? HIGH : LOW);
+  digitalWrite(redLedPedestrian, colorID == 2 ? HIGH : LOW);
+}
+
+tm localTime(){
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time. Restarting the board.");
+    delay(100);
+    ESP.restart();
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  return timeinfo;
+}
